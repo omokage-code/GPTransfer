a1c4d6027d060f65b4736538403557bdefee6829
omokage-code <280360457+omokage-code@users.noreply.github.com>
2026-06-27 21:08:49 +0900

    Fix app bundle signing for release packaging

--- scripts/build_app_bundle.sh
+++ scripts/build_app_bundle.sh
@@ -44,7 +44,7 @@ cat > "$CONTENTS_DIR/Info.plist" <<PLIST
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>CFBundleShortVersionString</key>
-    <string>0.1.0</string>
+    <string>0.1.1</string>
     <key>CFBundleVersion</key>
     <string>1</string>
     <key>LSMinimumSystemVersion</key>
@@ -59,5 +59,10 @@ cat > "$CONTENTS_DIR/Info.plist" <<PLIST
 </dict>
 </plist>
 PLIST
+
+xattr -cr "$APP_DIR"
+codesign --force --sign - "$MACOS_DIR/$HELPER_EXECUTABLE_NAME"
+codesign --force --sign - "$MACOS_DIR/$EXECUTABLE_NAME"
+codesign --force --sign - "$APP_DIR"
 
 echo "$APP_DIR"
